76197bf0934f4056a63dfc7124759d1bb09ec0c0
Jerald Lim <jeraldlim@Jeralds-MacBook-Pro.local>
2023-04-24 23:30:21 +0800

    fixed dependencies

--- src/main/java/app/CombinePDFPages.java
+++ src/main/java/app/CombinePDFPages.java
@@ -1,83 +1,84 @@
 package app;
 
+import java.io.Console;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 
 public class CombinePDFPages {
-    public static void main(String[] args) {
-        try {
-            // Load the first PDF document
-            PDDocument document1 = PDDocument.load(new File(args[0]));
 
-            // Load the second PDF document
-            PDDocument document2 = PDDocument.load(new File(args[1]));
+  public static void main(String[] args) {
+    System.out.println();
+    System.out.println("Let's merge some newly scanned PDFs!");
+    System.out.println("Reminder: You should not include '.pdf' in the file names");
+    System.out.println();
 
-            // Create a list to store the merged pages
-            List<PDPage> mergedPages = new ArrayList<>();
+    Console cons = System.console();
+    String doc1 = cons.readLine("First document name? >> ");
+    String doc2 = cons.readLine("Second document name? >> ");
+    String newDocName = cons.readLine("Name of merged pdf? >>");
+    String newDoc = newDocName + ".pdf";
+    System.out.println();
+    
+    try {
+        // Load the first PDF document
+        PDDocument document1 = PDDocument.load(new File(doc1 + ".pdf"));
 
-            // Get the number of pages in the first document
-            int numPages1 = document1.getNumberOfPages();
+        // Load the second PDF document
+        PDDocument document2 = PDDocument.load(new File(doc2 + ".pdf"));
 
-            // Get the number of pages in the second document
-            int numPages2 = document2.getNumberOfPages();
+        // Create a list to store the merged pages
+        List<PDPage> mergedPages = new ArrayList<>();
 
-            // // Determine the total number of pages in the merged document
-            // int numPages = numPages1 + numPages2;
+        // Get the number of pages in the first document
+        int numPages1 = document1.getNumberOfPages();
 
-            // check numPages of both documents are equal
-            if (numPages1 == numPages2) {
-                System.out.println(">>>> Number of pages are equal");
-            }
+        // Get the number of pages in the second document
+        int numPages2 = document2.getNumberOfPages();
 
+        // check numPages of both documents are equal
+        if (numPages1 != numPages2) {
+            System.out.println(">>>> Number of pages are not equal");
+            return;
+        } else {
             // Merge the pages from both documents
             for (int i = 0; i < numPages1; i++) {
-                // Add the next page from document 1
+            // Add the next page from document 1
                 if (i < numPages1) {
                     mergedPages.add(document1.getPage(i));
                 }
 
                 // Add the next page from document 2
                 if (i < numPages2) {
-                    mergedPages.add(document2.getPage(numPages2-1-i));
+                    mergedPages.add(document2.getPage(numPages2 - 1 - i));
                 }
-
-                // // Add the next page from document 1
-                // if (i + 1 < numPages1) {
-                //     mergedPages.add(document1.getPage(i + 1));
-                // }
-
-                // // Add the next page from document 2
-                // if (i + 1 < numPages2) {
-                //     mergedPages.add(document2.getPage(i + 1));
-                // }
             }
 
             // Create a new PDF document and add the merged pages
             PDDocument mergedDocument = new PDDocument();
             for (PDPage page : mergedPages) {
-                mergedDocument.addPage(page);
+            mergedDocument.addPage(page);
             }
 
             // Save the merged document to a file
             PDFMergerUtility merger = new PDFMergerUtility();
-            merger.setDestinationFileName("merged.pdf");
-            mergedDocument.save("merged.pdf");
+            merger.setDestinationFileName(newDoc);
+            mergedDocument.save(newDoc);
 
             // Close all open documents
             document1.close();
             document2.close();
             mergedDocument.close();
 
-            System.out.println(">>>> Documents merged successfully!");
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.printf(">>>> Documents merged successfully into %s!", newDoc);
+            System.out.println();
         }
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+  }
 }
-
